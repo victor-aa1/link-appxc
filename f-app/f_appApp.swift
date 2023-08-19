1ba6168e003f7ac0e481c2c7858b7964ad9689c8
Victor Agbeo <vicagbeo@outlook.com>
2023-08-19 13:22:05 -0700

    Initial Commit

---
+++ f-app/f_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  f_appApp.swift
+//  f-app
+//
+//  Created by vicagbeo on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct f_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
